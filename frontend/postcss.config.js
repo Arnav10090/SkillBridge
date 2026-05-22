7d503257c3b106e92371729b43d27654c1e426d4
Arnav Tiwari <arnavt292@gmail.com>
2026-05-22 15:38:28 +0530

    Fixed build issues

--- frontend/postcss.config.js
+++ frontend/postcss.config.js
@@ -1,13 +1,5 @@
 export default {
   plugins: {
     '@tailwindcss/postcss': {},
-    autoprefixer: {
-      overrideBrowserslist: [
-        'last 2 Chrome versions',
-        'last 2 Firefox versions',
-        'last 2 Safari versions',
-        'last 2 Edge versions',
-      ],
-    },
   },
 }
